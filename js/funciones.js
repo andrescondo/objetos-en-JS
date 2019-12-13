83ea4c396f81a730b9b5a7d987a15c947d67014c
Andres Condo <1dkevinandres.b@gmail.com>
2019-12-13 14:17:16 -0500

    primer commit

---
+++ js/funciones.js
@@ -0,0 +1,12 @@
+var name = 'Suany' ;
+
+function imprimirNameEnMayusculas(name)
+{
+	name = name.toUpperCase(name);
+	console.log(name)
+
+}
+
+imprimirNameEnMayusculas(name);
+
+console.log(name);
